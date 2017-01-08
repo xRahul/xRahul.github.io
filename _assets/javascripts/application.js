16bf0226ef6831a36a7f1bc5a3e75fb81c4525ee
Rahul Jain <rahul_jain@live.in>
2017-01-08 12:03:14 +0530

    refactored head include and fixed bug console async js

--- _assets/javascripts/application.js
+++ _assets/javascripts/application.js
@@ -17,3 +17,20 @@ sr.reveal('.reveal', {
   reset: false,
   viewFactor: 0
 });
+
+$(document).ready(function(){
+  if ($(".technology-list-a-svg").length) {
+    $(".technology-list-a-svg").hover(function(){
+        $(this).children(":first").css("top", "25%");
+        $(this).children(":first").css("left", "15%");
+        $(this).children(":first").css("width", "70%");
+        $(this).children(":first").css("height", "50%");
+      }, function(){
+        $(this).children(":first").css("top", "35%");
+        $(this).children(":first").css("left", "25%");
+        $(this).children(":first").css("width", "50%");
+        $(this).children(":first").css("height", "30%");
+      }
+    );
+  }
+});
