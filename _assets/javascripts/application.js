b8ebeb7803cbadae02041f92e942935d35decb39
Rahul Jain <rahul_jain@live.in>
2019-05-09 13:37:47 +0530

    Lazy Loading disqus

--- _assets/javascripts/application.js
+++ _assets/javascripts/application.js
@@ -32,3 +32,55 @@ $(document).ready(function(){
     );
   }
 });
+
+
+$(document).ready(function(){
+  if ($(".article-comments").length && $("#discus_identifier").val()) {
+    var options =
+    {
+      scriptUrl: '//' + $("#discus_identifier").val() + '.disqus.com/embed.js',
+      /*
+        @type: string (url)
+        @default: none
+        @required
+        URL of Disqus' executive JS file. The value is memorized on the first function call
+        and ignored otherwise because Disqus allows only one instance per page at the time.
+      */
+
+      laziness: 0,
+      /*
+        @type: int (>=0)
+        @default: 1
+        Sets the laziness of loading the widget: (viewport height) * laziness . For example:
+        0 - widget load starts when at the least a tiny part of it gets in the viewport;
+        1 - widget load starts when the distance between the widget zone and the viewport is no more than the height of the viewport;
+        2 - 2x viewports, etc.
+      */
+
+      throttle: 250,
+      /*
+        @type: int (milliseconds)
+        @default: 250
+        Defines how often the plugin should make calculations during the
+        processes such as resize of a browser's window or viewport scroll.
+        250 = 4 times in a second.
+      */
+
+      /*
+        @type: function
+        @default: none
+        Disqus-native options. Check Disqus' manual for more information.
+      */
+      // disqusConfig: function()
+      // {
+      //   this.page.title       = 'Page Title';
+      //   this.page.url         = 'http://url.to/your-website';
+      //   this.page.identifier  = 'unique-identifier';
+      // }
+    };
+
+    // jQuery
+    $.disqusLoader( '.disqus', options );
+  }
+});
+
--- _assets/javascripts/vendor.js
+++ _assets/javascripts/vendor.js
@@ -3,3 +3,4 @@
 //= require retinajs/dist/retina.js
 //= require scrollappear.js
 //= require zooming
+//= require jquery.disqusloader
